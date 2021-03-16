49528b7db18f36cdc9f59fe392216a9c5440c06a
Harshal Patil <harshpatilheros@gmail.com>
2021-03-16 05:23:56 +0000

    Band Name Generator

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Write your code below this line 👇
-n=input("Enter the Name?")
-length=len(n)
-print(length)
+print("Welcome To Band Name Generator.")
+city=input("What's Name of the city You Grep?")
+pet=input("What's You Pet Name?")
+print("You are Band Name Could Be ",city+pet)
