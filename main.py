b0e43fde2e7613b05edd353392a14e6a2477f54e
Harshal Patil <harshpatilheros@gmail.com>
2021-03-16 06:23:31 +0000

    Assignment Life Remaining

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-# Write your code below this line 👇
-n=input("Enter the Name?")
-length=len(n)
-print(length)
+age=int(input("Enter The Your Current Age="))
+years_rem= 90-age
+days_rem= years_rem*365
+months_rem= years_rem*12
+weeks_rem= years_rem*15
+message = f"You Have {days_rem} days,{weeks_rem} weeks,{months_rem} months left"
+print(message)
